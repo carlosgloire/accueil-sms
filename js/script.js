c3c768b48b1f41f3872bbbbbf6224dec4d178c36
unknown <ndayisababagloire96@gmail.com>
2023-11-19 04:36:01 +1100

    add this to my repository

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 document.getElementById("openmenu").addEventListener("click", function() {
     document.getElementById("menu").style.display = "block";
+    document.getElementById("openmenu").style.display = "none";
+    document.getElementById("closemenu").style.display = "block";
 });
 
 document.getElementById("closemenu").addEventListener("click", function(){
     document.getElementById("menu").style.display = "none";
+    document.getElementById("openmenu").style.display = "block";
+    document.getElementById("closemenu").style.display = "none";
 });
-
